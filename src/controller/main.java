a131186bb5c6cbbc9977ad2680c05407ca48c82c
Noe Podestá <podesta.noe@gmail.com>
2012-09-11 12:59:05 -0300

    Some examples

---
+++ src/controller/main.java
@@ -0,0 +1,18 @@
+package controller;
+
+import view.video;
+
+/**
+ *  @web http://jc-mouse.blogspot.com/
+ * @author Mouse
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new video().setVisible(true);
+    }
+
+}
